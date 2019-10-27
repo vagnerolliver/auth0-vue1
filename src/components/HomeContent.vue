7af233a869cd9c882c0b9dd4cb28e767c3c49525
vagner.olliver27 <vagner.oliveira@terceiros.zenvia.com>
2019-10-27 16:40:12 -0300

    comite na maste

--- src/components/HomeContent.vue
+++ src/components/HomeContent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="next-steps">
-    <h2 class="my-5 text-center">What can I do next?</h2>
+    <h2 class="my-5 text-center">Vou commitar na master</h2>
 
     <div class="row">
       <div class="col-md-5 mb-4">
